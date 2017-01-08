85ba87bc1b392b0e5a8ea31534f966d38a169155
jlhelwig <jeff.helwig@gmail.com>
2017-01-08 11:42:36 -0600

    update 2

--- main.js
+++ main.js
@@ -17,32 +17,51 @@ $(document).ready(function() {
         if ($xhr.status !== 200) {
             return;
         }
-
-        console.log(data);
+        console.log(data.Poster);
+        // var poster = "";
+        poster = data.Poster;
+        console.log(poster);
+        document.getElementById("q4").innerHTML = "<img src=" +poster+ " />" ;
+        // console.log(poster)
     });
 
-    $xhr.fail(function(err) {
-        console.log(err);
-    });
+//
 
-    var $xhr = $.getJSON('http://www.omdbapi.com/?t=the+martian');
-
-    $xhr.done(function(data) {
-        if ($xhr.status !== 200) {
-            return;
-        }
-
-        console.log(data);
-    });
-
-    $xhr.fail(function(err) {
-        console.log(err);
-    });
-
-
-
-
-
-
-
+//     $xhr.fail(function(err) {
+//         console.log(err);
+//     });
+//
+//     var $xhr = $.getJSON('http://www.omdbapi.com/?t=the+martian');
+//
+//     $xhr.done(function(data) {
+//         if ($xhr.status !== 200) {
+//             return;
+//         }
+//
+//         console.log(data);
+//     });
+//
+//     $xhr.fail(function(err) {
+//         console.log(err);
+//     });
+//
+//     var $xhr = $.getJSON('http://www.omdbapi.com/?t=fantastic+beasts');
+//
+//     $xhr.done(function(data) {
+//         if ($xhr.status !== 200) {
+//             return;
+//         }
+//
+//         console.log(data);
+//     });
+//
+//     $xhr.fail(function(err) {
+//         console.log(err);
+//     });
+//
+//
+//
+//
+//
+//
 });
